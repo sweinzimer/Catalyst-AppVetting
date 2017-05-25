72d55e245da59fef91b54773012b4647da9a66e0
ironmikesh <hainemic@oregonstate.edu>
2017-05-25 09:36:34 -0700

    Hide Show Advocate Section
    Added to hideShowApplication.js file handler for the Advocate section to
    hide/show...

--- public/javascripts/hideShowApplication.js
+++ public/javascripts/hideShowApplication.js
@@ -6,9 +6,15 @@ $(document).ready(function() {
     mortgage_up_to_date: null,
     restOfFormVisible: false,
   }
+  
+  var advocateState = {
+	  advo_bool: null,
+	  advocateFormVisible: false,
+  }
 
   $('[name=owns_home]').on('click', checkBothSelected)
   $('[name=mortgage_up_to_date]').on('click', checkBothSelected)
+  $('[name=advo_bool]').on('click', checkAdvocateSection)
 
   function checkBothSelected(e) {
     // Handle the event and update the state
@@ -35,7 +41,38 @@ $(document).ready(function() {
 
     }*/
   }
+  
+function checkAdvocateSection(e) {
+    // Handle the event and update the state
+    advocateState[e.target.name] = e.target.value
 
+    if (advocateState.advo_bool === "true" &&
+        advocateState.advocateFormVisible === false
+    ) {
+      // Make the advocate section visible
+      advocateState.advocateFormVisible = true
+      toggleAdvocateDisplay()
+    } 
+	else if (advocateState.advo_bool === "false" &&
+        advocateState.advocateFormVisible === true
+    ) {
+      // Hide the advocate section
+      advocateState.advocateFormVisible = false
+      toggleAdvocateDisplay()
+    } 
+	
+  }
+  function toggleAdvocateDisplay() {
+    $('.advoSection').each(function(i) {
+      // Skip the first, since that's the initial question being asked
+      // Those should always stay visible
+      //if (i > 0) {
+        var currentState = $(this).css('display')
+        $(this).css('display', (advocateState.advocateFormVisible ? 'inherit' : 'none'))
+      //}
+    })
+  }
+  
   function toggleFormSectionVisibility() {
     $('.formSection').each(function(i) {
       // Skip the first, since that's the initial question being asked
